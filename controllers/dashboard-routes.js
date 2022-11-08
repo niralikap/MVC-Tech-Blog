98253924b006ec08818f86f55fdeb01f204836b1
Nilu <nirali.rkapadia@gmail.com>
2022-11-08 15:39:09 -0800

    Updates the edit route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -75,7 +75,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
 
       const post = dbPostData.get({ plain: true });
 
-      res.render('edit-post', { post, loogedIn: true });
+      res.render('editPost', { post, loggedIn: true });
   })
   .catch(err => {
       console.log(err);
--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// User.hasMany(Post, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -14,10 +14,10 @@ Post.belongsTo(User, {
 
 
 
-// Comment.belongsTo(Post, {
-//   foreignKey: 'post_id',
-//   //onDelete: 'CASCADE'
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  //onDelete: 'CASCADE'
+});
 
 // User.hasMany(Comment, {
 //   foreignKey: 'user_id',
